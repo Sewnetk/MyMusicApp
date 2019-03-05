57afb03b45840d23bfab0eb70c5a558f4601a25a
Sewnetk <sewnet_tamene@yahoo.com>
2019-03-05 12:50:52 -0800

    Add project files.

---
+++ mediaplayerApp/CollectionOfmusics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace mediaplayerApp
+{
+    class CollectionOfmusics
+    {
+         public string Title { get; set; }
+        public string Album { get; set; }
+        public string Artist { get; set; }
+        public BitmapImage Albumpicture { get; set; }
+
+    }
+}
